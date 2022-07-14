e101f80e4b558e0b154921af5d9c4b4ab7d8196e
Kaosi Ezealigo <ezealigokosiso@gmail.com>
2022-07-14 16:23:58 +0100

    Adds functionality to dynamic edit buttons

--- script.js
+++ script.js
@@ -2,11 +2,14 @@ const addTodo = document.querySelector(".add");
 const list = document.querySelector(".list");
 const search = document.querySelector(".search input");
 
+let editItem;
+let editFlag = false;
+
 addTodo.addEventListener("submit", (e) => {
   e.preventDefault();
   let addText = addTodo.add.value;
 
-  if (addText) {
+  if (addText && !editFlag) {
     const element = document.createElement("li");
     element.classList.add("item");
 
@@ -18,35 +21,45 @@ addTodo.addEventListener("submit", (e) => {
           <i class="far fa-trash-alt delete"></i>
         </div>
         `;
+    const editBtn = element.querySelectorAll(".edit");
+    editBtn.forEach((btn) => {
+      btn.addEventListener('click', editFunc);
+    });
+
+    const delBtn = element.querySelectorAll('.delete');
+    delBtn.forEach((btn) => {
+      btn.addEventListener("click", delFunc);
+    });
 
     list.appendChild(element);
+
+    setDefault();
+  }else if(addText && editFlag){
+
+    editItem.innerHTML = addText;
 
     setDefault();
   }
 });
 
-list.addEventListener("click", (e) => {
-  const element = e.target.classList;
+// list.addEventListener("click", (e) => {
+//   const element = e.target.classList;
 
-  if (element.contains("delete")) {
-    e.target.parentElement.parentElement.remove();
-  }
+//   if (element.contains("delete")) {
+//     e.target.parentElement.parentElement.remove();
+//   }
 
-  if (element.contains("edit")) {
-    const editText = e.target.parentElement.parentElement.textContent.trim();
-    addTodo.add.value = editText;
-  }
-});
+//   if (element.contains("edit")) {
+//     const editText = e.target.parentElement.parentElement.textContent.trim();
+//     addTodo.add.value = editText;
+//   }
+// });
 
 search.addEventListener("keyup", () => {
   const term = search.value.toLowerCase().trim();
 
   searchBar(term);
 });
-
-function setDefault() {
-  addTodo.add.value = "";
-}
 
 function searchBar(term) {
   Array.from(list.children)
@@ -66,9 +79,21 @@ function searchBar(term) {
     });
 }
 
-// if(list.children.length <= 1){
-//   document.querySelector('.empty').style.display = 'block';
-// }
-// if(list.children.length > 1){
-//   document.querySelector('.empty').style.display = 'none';
-// }
+function editFunc(e){
+  editItem = e.currentTarget.parentElement.previousElementSibling;
+
+  addTodo.add.value = editItem.innerHTML;
+
+  editFlag = true;
+}
+
+function delFunc(e){
+  e.target.parentElement.parentElement.remove();
+
+  setDefault();
+}
+
+function setDefault() {
+  addTodo.add.value = "";
+  editFlag = false;
+}
